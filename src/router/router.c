0c8d8ba31ce05ecba57c56a810f4d8efce64d8a3
Lawrence Lipenga <lawrencelipenga2020@gmail.com>
2026-03-14 06:46:42 +0200

    Core: Revert Changes From /auth

--- src/router/router.c
+++ src/router/router.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "../services/auth/auth_service.h"
 #include "router/router.h"
 #include "http/http.h"
 #include "services/studentsdb.h"
